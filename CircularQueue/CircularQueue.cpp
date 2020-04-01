516b1186353d88b555d4a6c667ce0ea88283ef32
Ho-Lin Mark <holinlennymark@outlook.com>
2020-04-01 12:21:23 -0400

    Working version.

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -6,34 +6,32 @@
 
 int wmain(int argv, wchar_t const *argc[])
 {
-	{
-		sCircularQueue queue(32);
-
-		if (queue.ExtractFromOwnPool(4) == false)
-			return 0;
-		std::cout << "Max " << queue.max << '\n';
-		std::cout << "Extracted " << queue.number_of_entries << '\n';
-		queue.PutBackFromOwnPool();
-		if (queue.ExtractFromOwnPool(4) == false)
-			return 0;
-		std::cout << "Max " << queue.max << '\n';
-		std::cout << "Extracted " << queue.number_of_entries << '\n';
-		queue.PutBackFromOwnPool();
-		for (int i = 1; i < 39; ++i) {
-			if (queue.ExtractFromOwnPool(1) == false) {
-				std::cout << "Couldn't extract from pool. " << i << '\n';
-				queue.PutBackFromOwnPool();
-				break;
-			}
-			else {
-				std::cout << i << '\n';
-			}
+	sCircularQueue queue(32);
+	
+	if (queue.ExtractFromOwnPool(4) == false)
+		return 0;
+	std::cout << "Max " << queue.max << '\n';
+	std::cout << "Extracted " << queue.number_of_entries << '\n';
+	queue.PutBackFromOwnPool();
+	if (queue.ExtractFromOwnPool(4) == false)
+		return 0;
+	std::cout << "Max " << queue.max << '\n';
+	std::cout << "Extracted " << queue.number_of_entries << '\n';
+	queue.PutBackFromOwnPool();
+	for (int i = 1; i < 39; ++i) {
+		if (queue.ExtractFromOwnPool(1) == false) {
+			std::cout << "Couldn't extract from pool. " << i << '\n';
+			queue.PutBackFromOwnPool();
+			break;
 		}
-		std::cout << "Max " << queue.max << '\n';
-		std::cout << "Extracted " << queue.number_of_entries << '\n';
-		queue.PutBackFromOwnPool();
-		std::cout << "Max " << queue.max << '\n';
-		std::cout << "Extracted " << queue.number_of_entries << " from pool.\n";
+		else {
+			std::cout << i << '\n';
+		}
 	}
+	std::cout << "Max " << queue.max << '\n';
+	std::cout << "Extracted " << queue.number_of_entries << '\n';
+	queue.PutBackFromOwnPool();
+	std::cout << "Max " << queue.max << '\n';
+	std::cout << "Extracted " << queue.number_of_entries << " from pool.\n";
 	return 1;
 }
